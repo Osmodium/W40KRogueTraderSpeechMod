395bde08bdd48d2783a337b7c8988f0448ee144a
Osmodium <schubertchr@gmail.com>
2024-01-05 00:35:04 +0100

    Fix stopping, bound to CommonPCView atm

--- SpeechMod/Keybinds/PlaybackStop.cs
+++ SpeechMod/Keybinds/PlaybackStop.cs
@@ -1,6 +1,6 @@
 ﻿using HarmonyLib;
 using Kingmaker;
-using Kingmaker.Code.UI.MVVM.VM.ServiceWindows;
+using Kingmaker.Code.UI.MVVM.View.Common.PC;
 using ModConfiguration.Settings;
 
 namespace SpeechMod.Keybinds;
@@ -10,7 +10,7 @@ public class PlaybackStop : ModHotkeySettingEntry
     private const string _key = "playback.stop";
     private const string _title = "Stop playback";
     private const string _tooltip = "Stops playback of SpeechMod TTS.";
-    private const string _defaultValue = "%S;;WorldFullscreenUI;false";
+    private const string _defaultValue = "%S;;All;false";
     private const string BIND_NAME = $"{Constants.SETTINGS_PREFIX}.newcontrols.ui.{_key}";
 
     public PlaybackStop() : base(_key, _title, _tooltip, _defaultValue)
@@ -21,13 +21,13 @@ public class PlaybackStop : ModHotkeySettingEntry
     [HarmonyPatch]
     private static class Patches
     {
-        // TODO Bind in game to be used everywhere?
+        // TODO Does not work in loading screens, and thereby maybe more places?
         /// <summary>
-        /// Binds key after other service window keys are bound
+        /// Binds key after CommonPCView is initialized.
         /// </summary>
-        [HarmonyPatch(typeof(ServiceWindowsVM), nameof(ServiceWindowsVM.BindKeys))]
+        [HarmonyPatch(typeof(CommonPCView), nameof(CommonPCView.BindViewImplementation))]
         [HarmonyPostfix]
-        private static void Add(ServiceWindowsVM __instance)
+        private static void Add(CommonPCView __instance)
         {
             __instance.AddDisposable(Game.Instance.Keyboard.Bind(BIND_NAME, StopPlayback));
         }
