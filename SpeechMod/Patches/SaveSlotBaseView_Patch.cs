ea325e6cb5eb2c6efd77d939bbafb749549a7531
Osmodium <schubertchr@gmail.com>
2024-01-12 18:26:45 +0100

    Fix load screen tts

--- SpeechMod/Patches/SaveSlotBaseView_Patch.cs
+++ SpeechMod/Patches/SaveSlotBaseView_Patch.cs
@@ -1,30 +1,30 @@
 ﻿using HarmonyLib;
-using Kingmaker.Code.UI.MVVM.View.InfoWindow;
-using Kingmaker.Code.UI.MVVM.View.SaveLoad.Base;
-using Kingmaker.Code.UI.MVVM.VM.InfoWindow;
+using Kingmaker.UI.MVVM.View.SaveLoad.Base;
 using SpeechMod.Unity.Extensions;
+#if DEBUG
 using UnityEngine;
+#endif
 
 namespace SpeechMod.Patches;
 
 [HarmonyPatch]
 public class SaveSlotBaseView_Patch
 {
-    [HarmonyPatch(typeof(SaveSlotBaseView), nameof(SaveSlotBaseView.BindViewImplementation))]
+    [HarmonyPatch(typeof(SaveLoadBaseView), nameof(SaveLoadBaseView.BindViewImplementation))]
     [HarmonyPostfix]
-    public static void HookupLoadText(SaveSlotBaseView __instance)
+    public static void HookupLoadText(SaveLoadBaseView __instance)
     {
         if (!Main.Enabled)
             return;
 
 #if DEBUG
-        Debug.Log($"{nameof(InfoBaseView<InfoBaseVM>)}_SetPart_Postfix");
+        Debug.Log($"{nameof(SaveLoadBaseView)}_BindViewImplementation_Postfix");
 #endif
 
-        __instance.m_DlcRequiredDescription.HookupTextToSpeech();
-        __instance.m_DlcRequiredLabel.HookupTextToSpeech();
-        __instance.m_LocationLabel.HookupTextToSpeech();
-        __instance.m_NameLabel.HookupTextToSpeech();
-        __instance.m_TimeInGameLabel.HookupTextToSpeech();
+        __instance.m_DetailedSaveSlotView.m_NameLabel.HookupTextToSpeech();
+        __instance.m_DetailedSaveSlotView.m_LocationLabel.HookupTextToSpeech();
+        __instance.m_DetailedSaveSlotView.m_TimeInGameLabel.HookupTextToSpeech();
+        __instance.m_DetailedSaveSlotView.m_DlcRequiredLabel.HookupTextToSpeech();
+        __instance.m_DetailedSaveSlotView.m_DlcRequiredDescription.HookupTextToSpeech();
     }
 }
