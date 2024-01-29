631885609ff3280306689b7f5518131653d5d065
Osmodium <schubertchr@gmail.com>
2024-01-29 23:27:17 +0100

    Added modkit doesn't work... so messy

---
+++ ModKit/ModKit/ModKitSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace ModKit {
+    public partial class Mod {
+        public static ModKitSettings ModKitSettings;
+    }
+
+    public class ModKitSettings {
+        public static void Save() => Mod.modEntry.SaveSettings("ModKitSettings.json", Mod.ModKitSettings);
+        public static void Load() => Mod.modEntry.LoadSettings("ModKitSettings.json", ref Mod.ModKitSettings);
+
+        public int browserSearchLimit = 20;
+        public int browserDetailSearchLimit = 10;
+        public bool searchAsYouType = true;
+        public bool toggleKeyBindingsOutputToTranscript = true;
+        public bool toggleDataViewerShowNullAndEmpties = false;
+
+        // Localization
+        public string uiCultureCode = "en";
+    }
+}
